9186fb900f124d0784884ee0dcb8b777b5d3f9f5
AtmanDz <154727559+AtmanDz@users.noreply.github.com>
2025-04-17 17:48:37 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,49 @@
+const hamburger = document.querySelector(".hamburger");
+const navList = document.querySelector(".nav-list ul");
+const menu_item = document.querySelectorAll(
+  ".header .nav-bar .nav-list ul li a"
+);
+const header = document.querySelector(".header.container");
+
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("active");
+  navList.classList.toggle("active");
+});
+
+document.addEventListener("scroll", () => {
+  var scroll_position = window.scrollY;
+  if (scroll_position > 250) {
+    header.style.backgroundColor = "#29323c";
+  } else {
+    header.style.backgroundColor = "transparent";
+  }
+});
+
+menu_item.forEach((item) => {
+  item.addEventListener("click", () => {
+    hamburger.classList.remove("active");
+    navList.classList.remove("active");
+  });
+});
+
+// Animation smooth scroll pour les liens d'ancrage
+document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
+  anchor.addEventListener("click", function (e) {
+    e.preventDefault();
+    document.querySelector(this.getAttribute("href")).scrollIntoView({
+      behavior: "smooth",
+    });
+  });
+});
+
+// Animation au défilement
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("show");
+    }
+  });
+});
+
+const hiddenElements = document.querySelectorAll(".hidden");
+hiddenElements.forEach((el) => observer.observe(el));
